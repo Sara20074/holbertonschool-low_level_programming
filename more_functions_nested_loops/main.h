2993b3bc6e3ddf1f5573c6255bc91ba032ee41cb
Sara20074 <Sarakhalid.b28@gmail.com>
2026-03-02 12:18:34 +0000

    Add 101-print_number

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -11,6 +11,7 @@ void print_diagonal(int n);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
+void print_number(int n);
 void print_triangle(int size);
 void print_square(int size);
 
