cadeae35d16e821d4c8e4914aa56fc975ece61d4
Sara20074 <Sarakhalid.b28@gmail.com.com>
2026-03-04 12:03:16 +0000

    Fix magic line count

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -8,20 +7,16 @@
 int main(void)
 {
 	int n;
-	int a[5];
 	int *p;
-
-	a[2] = 1024;
+	int a[5];
+	a[0] = 1024;
+	a[1] = 402;
+	a[2] = 98;
+	a[3] = 6;
+	a[4] = 7;
 	p = &n;
-	/*
-	 * write your line of code here...
-	 * Remember:
-	 * - you are not allowed to use a
-	 * - you are not allowed to modify p
-	 * - only one statement
-	 * - you are not allowed to code anything else than this line of code
-	 */
-	p[5] = 98;
+	/* write your line of code here */
+	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
